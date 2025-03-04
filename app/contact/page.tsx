aadeeba68983e4fa3951ac75d1a463906667af2e
sh4sh4nk-9 <sashankchaubey2002@gmail.com>
2025-03-05 00:54:53 +0530

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -40,7 +40,7 @@ export default function Contact() {
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.6, delay: 0.3 }}
       >
-        nnehub@gmail.com
+        sashankchaubey2002@gmail.com
       </motion.a>
 
       
