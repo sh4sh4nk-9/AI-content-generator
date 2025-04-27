e26a8c4bc84d85346c4d580f01df8286a45a8c27
sh4sh4nk-9 <sashankchaubey2002@gmail.com>
2025-04-27 11:45:40 +0530

    fixes_key

--- utils/AiModel.tsx
+++ utils/AiModel.tsx
@@ -8,7 +8,7 @@ const {
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-2.0-flash",
+    model: "gemini-2.5-pro-preview-03-25",
   });
   
   const generationConfig = {
